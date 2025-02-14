6d9d88580b77b160aa099c5f2c4f02145373af29
James Love Ubuntu Laptop <jimmeylove@gmail.com>
2025-02-14 19:18:27 +0000

    add ML modle ( not using Tensorflow only tensors)

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import pickle
 import gzip
 from matplotlib import pyplot
 import numpy as np
+import torch
+import math
 
 DATA_PATH = Path("data")
 PATH = DATA_PATH / "mnist"
@@ -20,12 +22,75 @@ if not (PATH / FILENAME).exists():
         
 with gzip.open((PATH / FILENAME).as_posix(), "rb") as f:
         ((x_train, y_train), (x_valid, y_valid), _) = pickle.load(f, encoding="latin-1")
-        
-pyplot.imshow(x_train[0].reshape((28,28)), cmap="gray")
 
-try:
-    import google.colab
-except ImportError:
-    pyplot.show()
+# pyplot.imshow(x_train[0].reshape((28,28)), cmap="gray")
+# print(x_train.shape)
+
+x_train, y_train, x_valid, y_valid = map(
+    torch.tensor, (x_train, y_train, x_valid, y_valid)
+)
+n, c = x_train.shape
+
+print(x_train, y_train)
 print(x_train.shape)
+print(y_train.min(), y_train.max())
+
+weights = torch.randn(784, 10) / math.sqrt(784)
+weights.requires_grad_()
+bias = torch.zeros(10, requires_grad=True)
+
+def log_softmax(x):
+    return x - x.exp().sum(-1).log().unsqueeze(-1)
+
+def model(xb):
+    return log_softmax(xb @ weights + bias)
+
+bs = 64  # batch size
+
+xb = x_train[0:bs]  # a mini-batch from x
+preds = model(xb)  # predictions
+preds[0], preds.shape
+# print(preds[0], preds.shape)
+
+def nll(input, target):
+    return -input[range(target.shape[0]), target].mean()
+
+loss_func = nll
+
+yb = y_train[0:bs]
+print(loss_func(preds, yb))
+
+def accuracy(out, yb):
+    preds = torch.argmax(out, dim=1)
+    return (preds == yb).float().mean()
+
+print(accuracy(preds, yb))
+
+from IPython.core.debugger import set_trace
+
+lr = 0.5  # learning rate
+epochs = 2  # how many epochs to train for
+
+for epoch in range(epochs):
+    for i in range((n - 1) // bs + 1):
+        #         set_trace()
+        start_i = i * bs
+        end_i = start_i + bs
+        xb = x_train[start_i:end_i]
+        yb = y_train[start_i:end_i]
+        pred = model(xb)
+        loss = loss_func(pred, yb)
+
+        loss.backward()
+        with torch.no_grad():
+            weights -= weights.grad * lr
+            bias -= bias.grad * lr
+            weights.grad.zero_()
+            bias.grad.zero_()
+            
+print(loss_func(model(xb), yb), accuracy(model(xb), yb))
 # %%
+
+# ===================================================
+# got up to         Using torch.nn.functional
+# https://pytorch.org/tutorials/beginner/nn_tutorial.html#using-torch-nn-functional
