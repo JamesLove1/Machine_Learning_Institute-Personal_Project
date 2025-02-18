b901ced72357a088f9e4c12ed9244673a37fc65a
James Love Ubuntu Laptop <jimmeylove@gmail.com>
2025-02-18 14:57:23 +0000

    add py torch implentation

--- AI/main.py
+++ AI/main.py
@@ -1,4 +1,3 @@
-#%% 
 from pathlib import Path
 import requests
 import pickle
@@ -7,6 +6,11 @@ from matplotlib import pyplot
 import numpy as np
 import torch
 import math
+from torch import nn
+import torch.nn.functional as F
+from torch import optim 
+from torch.utils.data import TensorDataset
+
 
 DATA_PATH = Path("data")
 PATH = DATA_PATH / "mnist"
@@ -23,78 +27,57 @@ if not (PATH / FILENAME).exists():
 with gzip.open((PATH / FILENAME).as_posix(), "rb") as f:
         ((x_train, y_train), (x_valid, y_valid), _) = pickle.load(f, encoding="latin-1")
 
-pyplot.imshow(x_train[0].reshape((28,28)), cmap="gray")
-print(x_train.shape)
+# pyplot.imshow(x_train[0].reshape((28,28)), cmap="gray")
+# print(x_train.shape)
 
 x_train, y_train, x_valid, y_valid = map(
     torch.tensor, (x_train, y_train, x_valid, y_valid)
 )
+train_ds = TensorDataset(x_train, y_train)
 n, c = x_train.shape
 
-print(x_train, y_train)
-print(x_train.shape)
-print(y_train.min(), y_train.max())
+# print(x_train, y_train)
+# print(x_train.shape)
+# print(y_train.min(), y_train.max())
 
-weights = torch.randn(784, 10) / math.sqrt(784)
-weights.requires_grad_()
-bias = torch.zeros(10, requires_grad=True)
+lr = 0.5  # learning rate 
+epochs = 2 # how many epochs to train for
+bs = 64 # batch size
 
-def log_softmax(x):
-    return x - x.exp().sum(-1).log().unsqueeze(-1)
+loss_func = F.cross_entropy
 
-def model(xb):
-    return log_softmax(xb @ weights + bias)
+class Mnist_Logistic(nn.Module):
+    
+    def __init__(self):
+        super().__init__()
+        self.lin = nn.Linear(784, 10)
+        
+    def forward(self, xb):
+        return self.lin(xb)
+        
+def get_model():
+    model = Mnist_Logistic()
+    return model, optim.SGD(model.parameters(), lr=lr)
 
-bs = 64  # batch size
+model, opt = get_model()
 
-xb = x_train[0:bs]  # a mini-batch from x
-preds = model(xb)  # predictions
-preds[0], preds.shape
-# print(preds[0], preds.shape)
-
-def nll(input, target):
-    return -input[range(target.shape[0]), target].mean()
-
-loss_func = nll
-
-yb = y_train[0:bs]
-print(loss_func(preds, yb))
-
-def accuracy(out, yb):
-    preds = torch.argmax(out, dim=1)
-    return (preds == yb).float().mean()
-
-print(accuracy(preds, yb))
-
-# from IPython.core.debugger import set_trace
-
-lr = 0.5  # learning rate
-epochs = 2  # how many epochs to train for
-
-for epoch in range(epochs):
-    for i in range((n - 1) // bs + 1):
-        #         set_trace()
-        start_i = i * bs
-        end_i = start_i + bs
-        xb = x_train[start_i:end_i]
-        yb = y_train[start_i:end_i]
-        pred = model(xb)
-        loss = loss_func(pred, yb)
-
-        loss.backward()
-        with torch.no_grad():
-            weights -= weights.grad * lr
-            bias -= bias.grad * lr
-            weights.grad.zero_()
-            bias.grad.zero_()
+def fit(): # training loop
+    for epoch in range(epochs):
+        for i in range((n-1) // bs + 1):
+            xb, yb = train_ds[i * bs: i * bs + bs]            
+            pred = model(xb)
+            loss = loss_func(pred, yb)
             
-print(loss_func(model(xb), yb), accuracy(model(xb), yb))
-# %%
+            loss.backward()
+            opt.step()
+            opt.zero_grad()
 
+    print()
+    print(loss_func(model(xb), yb))
+    print()
+
+
+fit()
 # ===================================================
-# got up to         Using torch.nn.functional
-# https://pytorch.org/tutorials/beginner/nn_tutorial.html#using-torch-nn-functional
-
-print("hello world")
-
-
+# got up to         
+# https://pytorch.org/tutorials/beginner/nn_tutorial.html#refactor-using-dataset
