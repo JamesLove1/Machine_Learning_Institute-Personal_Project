842f1505f90b5c99d01646c209cb05e90dc3d273
James Love Ubuntu Laptop <jimmeylove@gmail.com>
2025-02-19 21:30:07 +0000

    add CNN modle

--- AI/mnist-model/modle.py
+++ AI/mnist-model/modle.py
@@ -3,46 +3,79 @@
 #  2. utilise https://pytorch.org/tutorials/beginner/nn_tutorial.html for insperation
 
 import torch
-from torch.utils.data import Dataset
-from torchvision import datasets
-from torchvision.transforms import ToTensor
-import matplotlib.pyplot as plt
+import torch.nn as nn
 from torch.utils.data import DataLoader
 
-# =======================
-#1.get data 
+from torchvision import datasets
+from torchvision.transforms import ToTensor
 
-trainData = datasets.MNIST(root="", train=True, download=True, transform=ToTensor())
-testData = datasets.MNIST(root="", train=False, download=True, transform=ToTensor())
+import matplotlib.pyplot as plt
+
+
+#1.build neural network 
+#    use CNN layers 
+
+class CCN_Network(nn.Module):
+    
+    def __init__(self):
+        super().__init__()
+    
+        # input layer 
+        self.conInputLayer = nn.Conv2d(in_channels=1, out_channels=16,kernel_size=3, stride=1, padding=1)
+        self.relu1 = nn.ReLU()
+        
+        # output layer 
+            
+    def forward(self, x):
+        
+        x = self.conInputLayer(x)
+        x = self.relu1(x)
+        return x 
+
+#2.get data 
+
+trainData = datasets.MNIST(root="", 
+                           train=True, 
+                           download=True, 
+                           transform=ToTensor()
+                           )
+
+testData = datasets.MNIST(root="", 
+                          train=False, 
+                          download=True, 
+                          transform=ToTensor()
+                          )
 
 train_dataLoader = DataLoader(trainData, batch_size=64, shuffle=True)
 test_dataLoader  = DataLoader(testData,  batch_size=64, shuffle=True)
 
-for i, batch in enumerate(test_dataLoader):
-    
-    print(i)
-    
-    if i >= 1:
-        break
-    
-    for i in range(0, len(batch[0])):
-        image = batch[0][i]
-        lable = batch[1][i]
-        # print(image)
-        # print(lable)
-
-        # img, label = train_dataLoader.dataset[1]
-        # plt.figure()
-        plt.title(lable)
-        plt.axis("off")
-        plt.imshow(image.squeeze(), cmap="gray")
-        plt.savefig("output.png")
-
-
-#2.build neural network 
-#    use CNN layers 
-
 #3.create loss function 
+
+
+
+
+
+
+
+# shape  torch.Size([batch=64, channels=1, width=28, height=28])
+
+model = CCN_Network()
+# print(model)
+
+images, lables = next(iter(train_dataLoader)) # for for loop when in prod 
+
+# plt.imshow(images[0].squeeze(), cmap="gray")
+# plt.savefig("input.png")
+
+output = model(images)
+print(output)
+
+# for i in range(0, len(output)):
+#     plt.imshow(output[i].detach().cpu().numpy(), cmap="gray")
+#     plt.savefig(f"output{i}.png")
+
+
+
 
 #4.create training loop 
 #   entrophy loss ? 
@@ -50,4 +83,23 @@ for i, batch in enumerate(test_dataLoader):
 
 #5.save model e.g. weights
 
+
+
+# ================
+# micilanious
+
+
+# prints images within batches 
+# for i, batch in enumerate(test_dataLoader):  
+    
+    # if i >= 1:
+    #     break
+    
+    # for i in range(0, len(batch[0])):
+    #     image = batch[0][i]
+    #     lable = batch[1][i]
+    #     plt.title(lable)
+    #     plt.axis("off")
+    #     plt.imshow(image.squeeze(), cmap="gray")
+    #     plt.savefig("output.png")
 
