e715924a54b1203c0ff614f441e1a9a76adc0fdb
James Love Ubuntu Laptop <jimmeylove@gmail.com>
2025-02-20 15:41:05 +0000

    add basic modle

--- AI/mnist-model/modle.py
+++ AI/mnist-model/modle.py
@@ -4,6 +4,7 @@
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.utils.data import DataLoader
 
 from torchvision import datasets
@@ -20,16 +21,24 @@ class CCN_Network(nn.Module):
     def __init__(self):
         super().__init__()
     
-        # input layer 
-        self.conInputLayer = nn.Conv2d(in_channels=1, out_channels=16,kernel_size=3, stride=1, padding=1)
+        # input layer - Feature Extraction 
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=16,kernel_size=3, stride=1, padding=1)
         self.relu1 = nn.ReLU()
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
         
-        # TODO : output layer 
+        # output layer 
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(in_features=16*14*14, out_features=10)
+            
             
     def forward(self, x):
         
-        x = self.conInputLayer(x)
+        x = self.conv1(x)
         x = self.relu1(x)
+        x = self.pool1(x)
+        x = self.flatten(x)
+        x = self.fc1(x)
+        
         return x 
 
 #2.get data 
@@ -52,49 +61,27 @@ test_dataLoader  = DataLoader(testData,  batch_size=64, shuffle=True)
 
 # TODO: 4.create loss function 
 
-
-#TODO: 5.create training loop 
-
+# TODO: 5.create training loop 
 #   entrophy loss ? 
 #   result given an input ?? 
 
-# shape  torch.Size([batch=64, channels=1, width=28, height=28])
-
-model = CCN_Network()
-# print(model)
-
-images, lables = next(iter(train_dataLoader)) # for for loop when in prod 
-
-# plt.imshow(images[0].squeeze(), cmap="gray")
-# plt.savefig("input.png")
-
-output = model(images)
-print(output)
-
-# for i in range(0, len(output)):
-#     plt.imshow(output[i].detach().cpu().numpy(), cmap="gray")
-#     plt.savefig(f"output{i}.png")
-
-
-#TODO: 6.save model e.g. weights
-
-
+# TODO: 6.save model e.g. weights
 
 # ================
 # micilanious
 
+# shape  torch.Size([batch=64, channels=1, width=28, height=28])
 
-# prints images within batches 
-# for i, batch in enumerate(test_dataLoader):  
-    
-    # if i >= 1:
-    #     break
-    
-    # for i in range(0, len(batch[0])):
-    #     image = batch[0][i]
-    #     lable = batch[1][i]
-    #     plt.title(lable)
-    #     plt.axis("off")
-    #     plt.imshow(image.squeeze(), cmap="gray")
-    #     plt.savefig("output.png")
+model = CCN_Network()
+
+images, lables = next(iter(train_dataLoader)) # for for loop when in prod 
+print("lables", lables)
+
+
+x = model(images)
+output = F.softmax(x, dim=1)
+finalOutput = torch.argmax(output, dim=1)
+print("final ", finalOutput)
+
+
 
