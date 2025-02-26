d22219571af1a7c3ccca1cf435e74be68fcc5ef9
James Love Ubuntu Laptop <jimmeylove@gmail.com>
2025-02-26 17:30:34 +0000

    updates

--- web/main.py
+++ web/main.py
@@ -11,13 +11,13 @@ if "img" not in st.session_state:
     st.session_state["img"] = []
 
 if "num" not in st.session_state:
-    st.session_state["num"] = None
+    st.session_state["num"] = ""
     
 if "res" not in st.session_state:
-    st.session_state["res"] = None
+    st.session_state["res"] = ""
     
 if "responseCode" not in st.session_state: 
-    st.session_state["responseCode"] = None
+    st.session_state["responseCode"] = ""
 
 def drawNumber():
     st.write("Draw a number")
@@ -34,13 +34,32 @@ def insertNumber():
         actualNumber = st.text_input("Actual Number", value="", key="predicted_number")  
         st.session_state["num"] = actualNumber
         
-
+        st.button("Predict Number",
+          key="predict_number",
+          on_click= sendRequst
+          )
+        
+        # st.write("Model Results")
+        st.write(f"Current prediction: {st.session_state.res}")
+        
+        
 def connectToDB():
     url = "postgresql://user:password@db:5432/user"
     engine = create_engine(url)
     return pd.read_sql("results", con=engine)
 
+def sendRequst():
+    
+    payload = { "img": st.session_state.img, "num": st.session_state.num}  
+    
+    response = res.post("http://ai:8000/", json=payload)
 
+    st.session_state["responseCode"] = response.status_code
+    
+    if st.session_state["responseCode"] == 200:
+        
+        resData = response.json()
+        st.session_state["res"] = resData["output"]
 
 # ========= page layout =========
 
@@ -59,26 +78,5 @@ with img:
 with submit:
     insertNumber()
 
-
-def sendRequst():
-    
-    payload = { "img": st.session_state.img, "num": st.session_state.num}  
-    
-    response = res.post("http://ai:8000/", json=payload)
-
-    st.session_state["responseCode"] = response.status_code
-    
-    if st.session_state["responseCode"] == 200:
-        
-        resData = response.json()
-        st.session_state["res"] = resData["output"]
-    
-st.button("Predict Number",
-          key="predict_number",
-          on_click= sendRequst
-          )
-
-st.write(f"output: from model: {st.session_state.res}")
-
-st.write("Model Results")
+st.write("Previous Model Results")
 st.table(df.tail(5))
